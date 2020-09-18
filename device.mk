2bb2f7f539cf094871064510e22c3df2d89b9891
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:56:31 -0700

    bluecross: Don't force secure ADB
    We need insecure ADB on eng builds to debug early boot failures. user
    and userdebug builds will still enforce secure ADB.
    
    Change-Id: I8c49ed881752adaf96dff6e6cfcc792455de5260
    Signed-off-by: nysascape <jago@nysascape.tech>

--- device.mk
+++ device.mk
@@ -856,9 +856,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.svn=37
 
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.adb.secure=1
-
 # pixel atrace HAL
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service.pixel
