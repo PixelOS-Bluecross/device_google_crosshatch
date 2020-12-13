6675c6ddb21fc8535924bf6e935f320e10bcf8ab
nysascape <jago@nysascape.tech>
2020-12-13 21:50:09 +0900

    bluecross: Build all display configurations from source
    Change-Id: I42e4d01f191e4cd31e94f1f98bd7b1ec124ef952
    Signed-off-by: nysascape <jago@nysascape.tech>

--- device.mk
+++ device.mk
@@ -891,6 +891,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 
 PRODUCT_PACKAGES += \
     libdisplayconfig \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.2 \
+    vendor.display.config@1.3 \
+    vendor.display.config@1.4 \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.6 \
+    vendor.display.config@1.7 \
+    vendor.display.config@1.8 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@1.1.vendor \
     vendor.display.config@1.2.vendor \
