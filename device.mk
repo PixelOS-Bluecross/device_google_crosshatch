0419aa7410be116a875c4b8ccf9c9aff5e9f59df
oliverharrison03 <oliverharrison03@gmail.com>
2021-12-10 15:25:53 +0000

    Remove google assistant prop

--- device.mk
+++ device.mk
@@ -941,9 +941,6 @@ PRODUCT_HOST_PACKAGES += brotli
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
-# Google Assistant
-PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
-
 # IPA config
 PRODUCT_PACKAGES += \
     IPACM_cfg.xml
